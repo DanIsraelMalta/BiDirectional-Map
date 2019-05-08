1b160222b605eca3d752ac25178cc291031a2e73
Dan Malta <malta.dan@gmail.com>
2019-05-08 08:01:40 +0300

    small update

--- ExecutionTimer.h
+++ ExecutionTimer.h
@@ -1,13 +1,19 @@
+#pragma once
+#include<chrono>
+#include <string>
+#include <iostream>
+
 // evaluate a a given code scope CPU run time
 template<class Resolution = std::chrono::milliseconds> struct ExecutionTimer {
     using Clock = std::conditional_t<std::chrono::high_resolution_clock::is_steady,
-                                     std::chrono::high_resolution_clock,
-                                     std::chrono::steady_clock>;
+        std::chrono::high_resolution_clock,
+        std::chrono::steady_clock>;
     Clock::time_point m_start;  // class construct time
-    
-    explicit constexpr ExecutionTimer() : m_start(Clock::now()) {}
+    std::string m_title{};
+
+    explicit constexpr ExecutionTimer(std::string title) : m_start(Clock::now()), m_title(title) {}
     ~ExecutionTimer() {
         const Clock::time_point end{ Clock::now() };
-        std::cout << " elapsed: " << std::chrono::duration_cast<Resolution>(end - m_start).count() << std::endl;
+        std::cout << m_title << " elapsed: " << std::chrono::duration_cast<Resolution>(end - m_start).count() << std::endl;
     }
 };
